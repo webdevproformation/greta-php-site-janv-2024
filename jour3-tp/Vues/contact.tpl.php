e05133d42492d4e832df4d9c5aeb0bd2b70f0bed
Malik H <malik.h@webdevpro.net>
2024-01-24 12:12:28 +0100

    page contact

---
+++ jour3-tp/Vues/contact.tpl.php
@@ -0,0 +1,29 @@
+<div class="container">
+
+    <h1>Vous avez une question ? Nous avons une réponse !!</h1>
+
+    <section class="row">
+        <div class="col-8">
+            <form>
+                <div class="form-floating mb-3">
+                    <input type="email" placeholder="votre@email.fr" class="form-control" id="email">
+                    <label for="email">votre@email.fr</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" placeholder="sujet" class="form-control" id="sujet">
+                    <label for="sujet">votre sujet</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <textarea id="commentaire" class="form-control" style="height:200px" placeholder="votre commentaire"></textarea>
+                    <label for="commentaire">votre commentaire</label>
+                </div>
+                <div class="text-end">
+                    <input type="submit" class="btn btn-outline-primary">
+                </div>
+            </form>
+        </div>
+        <div class="col">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9916261172198!2d2.29190101157724!3d48.85837007121255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sTour%20Eiffel!5e0!3m2!1sfr!2sfr!4v1706094645088!5m2!1sfr!2sfr" height="400" style="border:0;" allowfullscreen="" loading="lazy" class="w-100" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
+</div>
