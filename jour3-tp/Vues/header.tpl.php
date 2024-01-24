d0ca71f4dd716f81de42c15d54bf34f95d4deff4
Malik H <malik.h@webdevpro.net>
2024-01-24 11:03:38 +0100

    header et footer de notre site

---
+++ jour3-tp/Vues/header.tpl.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <header class="bg-primary">
+        <nav class="container navbar navbar-expand navbar-dark">
+            <span class="navbar-brand">
+                Mon premier MVC
+            </span>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?page=presentation" class="nav-link">Qui sommes nous ?</a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.php?page=contact" class="nav-link">Nous contacter</a>
+                </li>
+            </ul>
+        </nav>
+    </header>  
+    
