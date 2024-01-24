10bc8f3cbc1108e4ff7d97bb935886659c1a7be8
Malik H <malik.h@webdevpro.net>
2024-01-24 11:39:38 +0100

    code

---
+++ jour3-tp/Vues/presentation.tpl.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <h1>Qui sommes nous ???</h1>
+
+    <section class="row">
+        <div class="col">
+            <!-- p*5>lorem40 -->
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, voluptate maiores! Aliquam, distinctio quae dolor asperiores ab vitae totam, quam aspernatur voluptas sunt vero odio minus recusandae consectetur laudantium aut dolorum ea quidem ut facere enim obcaecati et! In, repellat?</p>
+            <p>Eligendi quaerat corrupti error corporis, perferendis maxime quos suscipit quae, beatae iste quas deserunt minus similique quo a id nemo exercitationem, magni dignissimos enim quam velit? Explicabo doloremque optio amet nemo. Omnis quod dolorum vero. Dolore minima doloribus rerum praesentium!</p>
+            <p>Qui dolor, temporibus quos, autem minus officia eius delectus fugiat molestias quam voluptates perspiciatis doloremque doloribus numquam magnam rem quia similique iste? Adipisci quia molestiae ratione porro repellendus eaque perferendis reiciendis ut nihil! Corrupti laboriosam distinctio sunt, ipsa accusantium voluptatibus.</p>
+            <p>Explicabo esse ex architecto necessitatibus voluptatibus. Vero corrupti porro, animi voluptate deserunt laboriosam itaque nobis? Odio rerum sequi facere non eaque, assumenda aliquam repellendus totam quia. Libero quaerat quibusdam nostrum temporibus. Natus, soluta? Aspernatur architecto nam saepe corrupti quod blanditiis?</p>
+            <p>Hic iure veritatis aspernatur asperiores maiores dignissimos sed commodi nam nemo ullam blanditiis vel eligendi sunt cupiditate praesentium, alias excepturi maxime fuga iste doloribus nostrum facere obcaecati animi. Ea rerum soluta ducimus dolores voluptatibus fuga cum aspernatur. Voluptates, numquam molestiae?</p>
+            <!-- p*5>lorem40 -->
+        </div>
+        <div class="col">
+            <img src="https://source.unsplash.com/random/600x100?v1" alt="" class="w-100 rounded mb-3">
+            <img src="https://source.unsplash.com/random/600x100?v2" alt="" class="w-100 rounded mb-3">
+            <img src="https://source.unsplash.com/random/600x100?v3" alt="" class="w-100 rounded mb-3">
+            <img src="https://source.unsplash.com/random/600x100?v4" alt="" class="w-100 rounded mb-3">
+        </div>
+    </section>
+
+</div>
