4cd16529c22adf1928ffef3842a5da9c39a7132b
Malik H <malik.h@webdevpro.net>
2024-01-24 12:16:02 +0100

    fin page de contact

--- jour3-tp/Vues/contact.tpl.php
+++ jour3-tp/Vues/contact.tpl.php
@@ -23,7 +23,7 @@
             </form>
         </div>
         <div class="col">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9916261172198!2d2.29190101157724!3d48.85837007121255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sTour%20Eiffel!5e0!3m2!1sfr!2sfr!4v1706094645088!5m2!1sfr!2sfr" height="400" style="border:0;" allowfullscreen="" loading="lazy" class="w-100" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9916261172198!2d2.29190101157724!3d48.85837007121255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sTour%20Eiffel!5e0!3m2!1sfr!2sfr!4v1706094645088!5m2!1sfr!2sfr" height="400" style="border:0;" allowfullscreen="" loading="lazy" class="w-100 rounded" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </section>
 </div>
