43bd0c547e031e1e0bf89a25982a5ea58668f58d
Malik H <malik.h@webdevpro.net>
2024-01-31 14:47:46 +0100

    page d'accueil

--- jour7-tp/src/Vues/home.tpl.php
+++ jour7-tp/src/Vues/home.tpl.php
@@ -1,2 +1,61 @@
-
+<div class="container">
+    <h1 class="display-4">Nos derniers articles à lire </h1>
+    <section class="row">
+        <!-- .col-4*6>article.card.mb-3>(h2.card-header.h4>lorem3)+img[src="https://via.placeholder.com/400x300"]+div.card-body>p>lorem20 -->
+        <div class="col-4">
+            <article class="card mb-3">
+                <h2 class="card-header h4">Lorem, ipsum dolor.</h2>
+                <img src="https://via.placeholder.com/400x300" alt="">
+                <div class="card-body">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quidem distinctio rerum vel consectetur ipsum quos provident dicta similique repellendus.</p>
+                </div>
+            </article>
+        </div>
+        <div class="col-4">
+            <article class="card mb-3">
+                <h2 class="card-header h4">Deserunt, sit cupiditate?</h2>
+                <img src="https://via.placeholder.com/400x300" alt="">
+                <div class="card-body">
+                    <p>Aspernatur cupiditate rem facilis nam, nesciunt reiciendis at sunt possimus unde deleniti fugit eum asperiores amet enim totam perferendis quas.</p>
+                </div>
+            </article>
+        </div>
+        <div class="col-4">
+            <article class="card mb-3">
+                <h2 class="card-header h4">Voluptas, a eius!</h2>
+                <img src="https://via.placeholder.com/400x300" alt="">
+                <div class="card-body">
+                    <p>Sequi eaque ex mollitia commodi nisi. Sed illum labore delectus. Deserunt error tempore accusantium reprehenderit, laboriosam iusto ut iste doloremque!</p>
+                </div>
+            </article>
+        </div>
+        <div class="col-4">
+            <article class="card mb-3">
+                <h2 class="card-header h4">Molestiae, laborum quam.</h2>
+                <img src="https://via.placeholder.com/400x300" alt="">
+                <div class="card-body">
+                    <p>Ipsa molestias blanditiis, ipsam magnam ullam placeat numquam reiciendis, alias sed quaerat veritatis cupiditate velit magni non aliquid earum aut!</p>
+                </div>
+            </article>
+        </div>
+        <div class="col-4">
+            <article class="card mb-3">
+                <h2 class="card-header h4">Odit, distinctio aut.</h2>
+                <img src="https://via.placeholder.com/400x300" alt="">
+                <div class="card-body">
+                    <p>Perspiciatis culpa recusandae, qui officia unde harum necessitatibus nisi dolor suscipit omnis tenetur, tempora pariatur incidunt fugiat, ipsa explicabo alias.</p>
+                </div>
+            </article>
+        </div>
+        <div class="col-4">
+            <article class="card mb-3">
+                <h2 class="card-header h4">Consectetur, molestiae tenetur?</h2>
+                <img src="https://via.placeholder.com/400x300" alt="">
+                <div class="card-body">
+                    <p>Aliquid sequi maiores hic quasi quos ea porro, voluptatum amet. Enim necessitatibus quis repellendus delectus sed tempora ducimus, placeat sit!</p>
+                </div>
+            </article>
+        </div>
+    </section>
+</div>
    
