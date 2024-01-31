a25f0af1b6f51b97252455299086873031ddeb7f
Malik H <malik.h@webdevpro.net>
2024-01-31 14:50:34 +0100

    lien vers les articles

--- jour7-tp/src/Vues/home.tpl.php
+++ jour7-tp/src/Vues/home.tpl.php
@@ -9,6 +9,9 @@
                 <div class="card-body">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quidem distinctio rerum vel consectetur ipsum quos provident dicta similique repellendus.</p>
                 </div>
+                <div class="card-footer text-end">
+                    <a href="<?= $router->generate("article" , ["id" => 1]) ?>" class="btn btn-primary">lire la suite</a>
+                </div>
             </article>
         </div>
         <div class="col-4">
