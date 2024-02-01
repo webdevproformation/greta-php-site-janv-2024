39e6814d5d6e434ff566d52c4a7755f0a26840fe
Malik H <malik.h@webdevpro.net>
2024-02-01 09:58:50 +0100

    controller fait

---
+++ jour7-tp/src/Controller/AdminController.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\Controller;
+
+class AdminController  extends AbstractController{
+
+    public function article_new(){
+        $data = [];
+        $data["h1"] = "créer un nouvel article";
+        $data["title"] = "créer un nouvel article";
+        $this->render("article_new", $data); 
+    }
+}
--- jour7-tp/src/Controller/SiteController.php
+++ jour7-tp/src/Controller/SiteController.php
@@ -1,26 +1,13 @@
 <?php 
-
 namespace App\Controller ;
-
 use App\Model\Article;
-
-class SiteController{
-
+class SiteController extends AbstractController{
     public function home() :void {
         $data = [];
         $articleModel =  new Article();
         $data["articles"] = $articleModel->readAll();
         $data["h1"] = "Nos derniers articles à lire";
         $data["title"] = "page d'accueil";
-
         $this->render("home" , $data);
     }
-
-    private function render(string $template , array $data):void{
-        global $router ; // va récupérer la variable $router qui a été créée dans le contexte global 
-        require_once __DIR__ . "/../Vues/header.tpl.php";
-        require_once __DIR__ . "/../Vues/$template.tpl.php";
-        require_once __DIR__ . "/../Vues/footer.tpl.php";
-    }
-
 }
