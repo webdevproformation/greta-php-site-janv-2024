aaead91ef965588844125d4b931a7baabb530f13
Malik H <malik.h@webdevpro.net>
2024-02-05 10:29:04 +0100

    bootstrap et les variables css

--- jour7-tp/src/Vues/header.tpl.php
+++ jour7-tp/src/Vues/header.tpl.php
@@ -5,6 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap" rel="stylesheet">
+
+    <style>
+        :root{
+            /* variable css 
+            https://developer.mozilla.org/fr/docs/Web/CSS/Using_CSS_custom_properties
+            https://caniuse.com/css-variables
+            */ 
+            --bs-font-sans-serif : "Protest Riot", sans-serif;
+            --bs-heading-color : pink ; 
+        }
+
+
+    </style>
+
   </head>
   <body>
     <header class="bg-warning">
