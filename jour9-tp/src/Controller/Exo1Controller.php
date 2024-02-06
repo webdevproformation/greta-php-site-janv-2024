eb52a7cd79107c274bef298bab9ab2622a7b7eac
Malik H <malik.h@webdevpro.net>
2024-02-06 11:53:55 +0100

    correction

--- jour9-tp/src/Controller/Exo1Controller.php
+++ jour9-tp/src/Controller/Exo1Controller.php
@@ -8,4 +8,14 @@ class Exo1Controller extends AbstractController{
         // http://192.168.15.22/jour9-tp/exo1
         dd("j'ai réussi l'exo 1");
     }
+
+    #[Route(path : "/exo2" , name:"exo2")]
+    public function exo2(){
+        $data = [
+            "prenom" => "Alain",
+            "nom" => "Doe",
+            "age" => 22
+        ];
+        return $this->render("exo/vue.html.twig" , $data); 
+    }
 }
