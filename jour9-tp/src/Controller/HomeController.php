dfeeae8012e5873b33827ced768d1f7f04f74f9b
Malik H <malik.h@webdevpro.net>
2024-02-06 11:27:11 +0100

    créer une vue et lui envoyer des infos et l'afficher

--- jour9-tp/src/Controller/HomeController.php
+++ jour9-tp/src/Controller/HomeController.php
@@ -16,4 +16,15 @@ class HomeController extends AbstractController {
         $this->render("front/home.html.twig", $data);
     }
 
+    // uri  /description
+    // nom de la route : premiere-page
+    #[Route("/description", name:"premiere-page")]
+    public function description(){
+        $data["h1"] = "voici le titre de la page" ; 
+        return $this->render("front/description.html.twig" , $data); 
+        // dans templates
+        // créer un dossier front 
+        // dans ce dossier créer un fichier 
+        // dont l'extension est .html.twig
+    }
 }
